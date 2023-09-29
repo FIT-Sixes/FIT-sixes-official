819c0c14a3bfa57b0ae671c73e0dd5baebff196f
vinod liyanage <vinodsliyanage@gmail.com>
2023-09-29 12:42:18 +0530

    add basic page structure.

--- src/pages/landing-page/LandingPage.jsx
+++ src/pages/landing-page/LandingPage.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const LandingPage = () => {
-    return <div>LandingPage</div>;
+    return (
+        <>
+            <section>{/* hero */}</section>
+
+            <section>{/* countdown */}</section>
+
+            <section>{/* about */}</section>
+
+            <section>{/* awards */}</section>
+
+            <section>{/* teams */}</section>
+
+            <section>{/* sponsors */}</section>
+        </>
+    );
 };
 
 export default LandingPage;
