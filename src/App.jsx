7e7871ee65e13fd628909dc5520219a07342185f
vinod liyanage <vinodsliyanage@gmail.com>
2023-09-29 22:46:34 +0530

    fix the background straching issue.

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,15 @@ import Router from './routes';
 const App = () => {
     return (
         <BrowserRouter>
-            <div className="w-full bg-gradient-pattern bg-repeat-y bg-cover bg-center">
-                
+            <div className="w-full bg-gradient-pattern bg-repeat-y bg-contain bg-center">
                 <h1 className="w-full px-6 py-3 mb-2 font-mono font-extrabold text-center bg-yellow-400">
                     FIT Sixes 2K23 | Developed by Batch 20
                 </h1>
 
                 {/* example sections */}
+                <section className="h-[100vh]"></section>
+                <section className="h-[100vh]"></section>
+                <section className="h-[100vh]"></section>
                 <section className="h-[100vh]"></section>
                 <section className="h-[100vh]"></section>
                 <section className="h-[100vh]"></section>
