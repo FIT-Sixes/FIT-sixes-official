31375a930c77e107d5e3822162a66a41b7bdd9aa
Thushan D. Fernando <thushandfdo@gmail.com>
2023-09-30 19:48:21 +0530

    scrollbar style

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,12 @@ export default {
                     ${theme('colors.gradient-top')} 3.65%, 
                     ${theme('colors.gradient-middle')} 20.31%, 
                     ${theme('colors.gradient-bottom')} 100%
-                ), url(./src/assets/pattern.webp);`,
+                ), url(./src/assets/pattern.webp);`
             }),
             colors: {
                 'gradient-top': '#07334Efa',
                 'gradient-middle': '#151632fa',
-                'gradient-bottom': '#151632fa',
+                'gradient-bottom': '#151632fa'
             },
         },
     },
