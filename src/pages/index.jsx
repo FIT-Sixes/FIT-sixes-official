1fb9f14c8e6c8058e3c8647070ea20c324c2fd0c
Vinod Liyanage <vinodsliyanage@gmail.com>
2023-09-30 23:45:15 +0530

    add AboutPage.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
 import ExamplePage from './examples/ExamplePage/ExamplePage';
 import LandingPage from './LandingPage/LandingPage';
 import ContactUsPage from './ContactUsPage/ContactUsPage';
+import AboutPage from './AboutPage/AboutPage';
 
-export { ExamplePage, LandingPage, ContactUsPage };
+export { ExamplePage, LandingPage, ContactUsPage, AboutPage };
