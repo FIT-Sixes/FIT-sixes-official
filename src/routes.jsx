9ba5268626414fdaee5da7e8cb9008b91b1a4a2a
Vinod Liyanage <vinodsliyanage@gmail.com>
2023-09-30 23:45:05 +0530

    add AboutPage route.

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,13 +1,17 @@
 import { useRoutes } from 'react-router-dom';
 
-// local imports 
-import { ExamplePage, LandingPage, ContactUsPage } from './pages';
+// local imports
+import { ExamplePage, LandingPage, ContactUsPage, AboutPage } from './pages';
 
 const Router = () => {
     return useRoutes([
         {
             path: '/',
             element: <LandingPage />,
+        },
+        {
+            path: '/about',
+            element: <AboutPage />,
         },
         {
             path: '/sample',
