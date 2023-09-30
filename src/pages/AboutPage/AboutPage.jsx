b8614fa00b021cddd108e7d25076ebebf8672dcd
Vinod Liyanage <vinodsliyanage@gmail.com>
2023-09-30 23:43:15 +0530

    add AboutPage structure.

---
+++ src/pages/AboutPage/AboutPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <>
+        <section>{/* About */}</section>
+        <section>{/* Shawcase */}</section>
+        <section>{/* History */}</section>
+    </>
+  )
+}
+
+export default AboutPage
