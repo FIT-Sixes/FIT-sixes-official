dcb6344833ce194d2790babd72efd1fa0dd197df
Vinod Liyanage <vinodsliyanage@gmail.com>
2023-10-01 10:55:41 +0530

    add images.

---
+++ src/constants/images.js
@@ -0,0 +1,17 @@
+import image_2k22 from '../assets/history-card-images/2k22.png';
+import image_2k19 from '../assets/history-card-images/2k19.png';
+import image_2k18 from '../assets/history-card-images/2k18.png';
+import image_2k17 from '../assets/history-card-images/2k17.png';
+import image_2k16 from '../assets/history-card-images/2k16.png';
+
+const images = {
+    historyCard: {
+        image_2k22,
+        image_2k19,
+        image_2k18,
+        image_2k17,
+        image_2k16,
+    },
+};
+
+export default images;
