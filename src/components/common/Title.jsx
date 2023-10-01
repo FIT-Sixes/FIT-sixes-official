516689a26c124e696eff338ccf3b46d8895b457f
Vinod Liyanage <vinodsliyanage@gmail.com>
2023-10-01 11:16:06 +0530

    add shadowTitle prop.

--- src/components/common/Title.jsx
+++ src/components/common/Title.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Title = ({ title }) => {
+const Title = ({ title, shadowTitle = title }) => {
     return (
         <div className="flex flex-col justify-center items-center sm:-mb-5 md:-mb-10">
             <h3 className="text-2xl uppercase font-bold tracking-widest text-white -mb-8 md:text-4xl md:-mb-16">
                 {title}
             </h3>
             <h1 className="text-7xl uppercase font-bold tracking-widest text-accent opacity-10 sm:text-8xl md:text-10xl">
-                {title}
+                {shadowTitle}
             </h1>
         </div>
     );
