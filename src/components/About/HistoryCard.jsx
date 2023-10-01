7d69876584cc5750774735790ecfc024315a466b
Vinod Liyanage <vinodsliyanage@gmail.com>
2023-10-01 11:19:25 +0530

    update background color.

--- src/components/About/HistoryCard.jsx
+++ src/components/About/HistoryCard.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const HistoryCard = ({ title, image, alt }) => {
     return (
-        <div className="w-[180px] flex flex-col justify-center items-center p-2 rounded-5xl bg-slate-50 bg-opacity-40 md:w-[200px]">
-            <img className='w-full object-cover' src={image} alt={alt} />
+        <div className="w-[180px] flex flex-col justify-center items-center p-2 rounded-5xl bg-blue-gray-50 bg-opacity-40 md:w-[200px]">
+            <img className="w-full object-cover" src={image} alt={alt} />
             <h3 className="uppercase text-3xl text-white font-bold my-2">
                 {title}
             </h3>
