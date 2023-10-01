cdebfd27cd1a59a1aaacf67a42f7f3ae4a25fb73
Vinod Liyanage <vinodsliyanage@gmail.com>
2023-10-01 10:55:24 +0530

    add historyCards data.

---
+++ src/constants/data.js
@@ -0,0 +1,31 @@
+import images from './images';
+
+const historyCards = [
+    {
+        title: '2K22',
+        image: images.historyCard.image_2k22,
+        alt: '2K22',
+    },
+    {
+        title: '2K19',
+        image: images.historyCard.image_2k19,
+        alt: '2K19',
+    },
+    {
+        title: '2K18',
+        image: images.historyCard.image_2k18,
+        alt: '2K18',
+    },
+    {
+        title: '2K17',
+        image: images.historyCard.image_2k17,
+        alt: '2K17',
+    },
+    {
+        title: '2K16',
+        image: images.historyCard.image_2k16,
+        alt: '2K16',
+    },
+];
+
+export { historyCards };
