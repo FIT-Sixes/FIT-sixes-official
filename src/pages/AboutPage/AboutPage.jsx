9fcfc46a4cc50500f34888d71e443960f28e28fe
Vinod Liyanage <vinodsliyanage@gmail.com>
2023-10-01 10:47:14 +0530

    create AboutPage.

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React from 'react';
+
+import HistoryCard from '../../components/About/HistoryCard';
+import Title from '../../components/common/Title';
+
+import { historyCards } from '../../constants/data';
 
 const AboutPage = () => {
-  return (
-    <>
-        <section>{/* About */}</section>
-        <section>{/* Shawcase */}</section>
-        <section>{/* History */}</section>
-    </>
-  )
-}
+    return (
+        <>
+            <section className="h-[100vh]">{/* About */}</section>
+            <section className="h-[100vh]">{/* Shawcase */}</section>
 
-export default AboutPage
+            <section className="flex flex-col items-center my-72">
+                <Title title="history"/>
+
+                <div className="flex flex-row gap-3 flex-wrap justify-center items-center">
+                    {historyCards.map((item, index) => (
+                        <HistoryCard key={index} {...item} />
+                    ))}
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default AboutPage;
